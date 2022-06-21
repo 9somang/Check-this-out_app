1e9094e8613016bf5f45b00821e16a58c7f5e112
ksmang09 <ksmang09@naver.com>
2022-06-21 20:50:21 +0900

    test commit
    123

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:check_app/homepage.dart';
 import 'package:check_app/user/Login.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData(fontFamily: 'Jua'),
-      home: LoginPage(),
+      home: Homepage(),
     );
 
   }
